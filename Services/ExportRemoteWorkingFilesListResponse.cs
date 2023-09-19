2ed14caafc0387e6a93f73c1009cff96babe914f
Maxime LAPLANCHE <mlaplanche@sepro-group.com>
2023-09-19 16:28:33 +0200

    feat: add RemoteWorkingFileServiceClient endpoint

---
+++ Services/ExportRemoteWorkingFilesListResponse.cs
@@ -0,0 +1,14 @@
+﻿//.ExportAbsenceFilesLimitedToAPeriodResponse
+
+
+using System.Xml.Serialization;
+using KelioSDK.Models;
+
+namespace KelioSDK.Services;
+
+[XmlType(Namespace = "http://echange.service.open.bodet.com", TypeName = "exportRemoteWorkingFilesListResponse")]
+public class ExportRemoteWorkingFilesListResponse
+{
+    [XmlArray(ElementName = "exportRemoteWorkingFilesList", IsNullable = false, Order = 0)]
+    public List<RemoteWorkingFile> ExportRemoteWorkingFilesList { get; set; }
+}
---
+++ Services/RemoteWorkingFileServiceClient.cs
@@ -0,0 +1,37 @@
+﻿//.RemoteWorkingFileServiceClient
+
+
+using KelioSDK.Common;
+
+namespace KelioSDK.Services;
+
+public class RemoteWorkingFileServiceClient
+{
+    private const string Service = "RemoteWorkingFileService";
+    private readonly string _baseuri;
+    private readonly string _password;
+    private readonly int _timeoutSeconds;
+    private readonly string _username;
+
+    public RemoteWorkingFileServiceClient(string username, string password, string baseuri, int timeoutSeconds = 60)
+    {
+        _username = username;
+        _password = password;
+        _baseuri = baseuri;
+        _timeoutSeconds = timeoutSeconds;
+    }
+
+    public ExportRemoteWorkingFilesListResponse ExportRemoteWorkingFilesList(
+        ExportRemoteWorkingFilesList arg)
+    {
+        return Client.Post<ExportRemoteWorkingFilesList, ExportRemoteWorkingFilesListResponse>(
+            _username, _password, _baseuri, Service, arg, _timeoutSeconds);
+    }
+
+    public Task<ExportRemoteWorkingFilesListResponse> ExportRemoteWorkingFilesListAsync(
+        ExportRemoteWorkingFilesList arg)
+    {
+        return Client.PostAsync<ExportRemoteWorkingFilesList, ExportRemoteWorkingFilesListResponse>(
+            _username, _password, _baseuri, Service, arg, _timeoutSeconds);
+    }
+}
